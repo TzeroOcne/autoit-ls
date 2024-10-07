964d49a2169016ad3ff3ac23bd19fb60a23d8f77
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-10-08 01:31:52 +0700

    feat(main): implement listening to stdin

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,17 @@
 const std = @import("std");
-const rpc = @import("./rpc/rpc.zig");
 
-pub fn main() void {
-    const result = rpc.decodeMessage("Content-Length: 36\r\n\r\n{\"method\":\"textDocument/completion\"}") catch |err| {
-        std.log.err("{!}", .{err});
-        return;
-    };
-    std.log.debug("{any}\n", .{result});
+pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
+
+    while (true) {
+        const result = try stdin.readUntilDelimiterOrEofAlloc(std.heap.page_allocator, '\n', 1024);
+        if (result == null) break;
+
+        const line = result.?;
+        defer std.heap.page_allocator.free(line);
+        if (line.len == 0) break;
+
+        // Do something with `line`
+        std.debug.print("Read: {s}\n", .{line});
+    }
 }
