8a455d377aec4012a4c91e2a60305f78916def04
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-10-07 22:05:27 +0700

    feat(rpc): return message instead of print

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
+const std = @import("std");
 const rpc = @import("./rpc/rpc.zig");
 
-pub fn main() !void {
-    rpc.encodeMessage(.{ 1, 2 });
+pub fn main() void {
+    const result = rpc.encodeMessage(.{ 1, 2 }) catch &[_]u8{};
+    std.log.debug("{s}\n", .{result});
 }
--- src/rpc/rpc.zig
+++ src/rpc/rpc.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
 
-pub fn encodeMessage(message: anytype) void {
-    var buf: [4096]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buf);
-    var string = std.ArrayList(u8).init(fba.allocator());
-    std.json.stringify(message, .{}, string.writer()) catch {};
-    std.log.info("{s}\n", .{string.items});
+pub fn encodeMessage(message: anytype) ![]u8 {
+    const allocator = std.heap.page_allocator;
+    var buffer = std.ArrayList(u8).init(allocator);
+
+    try std.json.stringify(message, .{}, buffer.writer());
+
+    return buffer.items;
 }
