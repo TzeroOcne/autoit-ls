c5318df668fd908e19cc9b876f21afdd7ee59a92
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-10-10 17:27:44 +0700

    feat(main): add reader

--- src/main.zig
+++ src/main.zig
@@ -2,23 +2,27 @@ const std = @import("std");
 const logger = @import("./logger/logger.zig");
 
 pub fn main() !void {
-    const stdin = std.io.getStdIn().reader();
     try logger.init();
+    try logger.println("Starting AutoIt Language Server");
+
+    const stdin = std.io.getStdIn();
+    var buf = std.io.bufferedReader(stdin.reader());
+    var reader = buf.reader();
+    var msg_buf: [8192]u8 = undefined;
 
     var message: []u8 = "";
     while (true) {
-        const result = try stdin.readUntilDelimiterOrEofAlloc(std.heap.page_allocator, '\n', 1024);
-        if (result == null) break;
-
-        const line = result.?;
-        message = try std.mem.concat(std.heap.page_allocator, u8, &[_][]const u8{ message, line });
-        try logger.println(line);
-        if (try isReady(message)) {
-            message = "";
+        const result = try reader.readUntilDelimiterOrEof(&msg_buf, '\n');
+        if (result) |line| {
+            try logger.println(line);
+            message = try std.mem.concat(std.heap.page_allocator, u8, &[_][]const u8{ message, line });
+            if (try isReady(message)) {
+                message = "";
+            }
         }
-        defer std.heap.page_allocator.free(line);
-        if (line.len == 0) break;
     }
+
+    try logger.println("Stopped AutoIt Language Server");
 }
 
 fn isReady(message: []const u8) !bool {
