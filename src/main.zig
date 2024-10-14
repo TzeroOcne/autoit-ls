56da7de907f5f51ca1b5d7cb2a47efccaf3519eb
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-10-14 18:41:45 +0700

    feat(reader): add content reader after header

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,10 @@ const std = @import("std");
 const logger = @import("./logger/logger.zig");
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{ .stack_trace_frames = 8 }){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
     try logger.init();
     try logger.println("Starting AutoIt Language Server");
 
@@ -12,23 +16,39 @@ pub fn main() !void {
     var header_buffer: [1024]u8 = undefined;
     var header_stream = std.io.fixedBufferStream(&header_buffer);
 
+    var content_buffer = std.ArrayList(u8).init(allocator);
+    defer content_buffer.deinit();
+
+    const max_content_length = 4 << 20; // 4MB
+
     while (true) {
-        const header: []const u8 = blk: {
+        const header = blk: {
             header_stream.reset();
             while (!std.mem.endsWith(u8, header_buffer[0..header_stream.pos], "\r\n\r\n")) {
                 try reader.streamUntilDelimiter(header_stream.writer(), '\n', null);
                 _ = try header_stream.write("\n");
             }
-            break :blk header_buffer[0..header_stream.pos];
+            break :blk try parseHeaders(header_buffer[0..header_stream.pos]);
         };
 
-        try logger.println(header);
+        const header_string = try std.fmt.allocPrint(std.heap.page_allocator, "Length: {}", .{header.content_length});
+        try logger.println(header_string);
+
+        const content = blk: {
+            if (header.content_length > max_content_length) return error.MessageTooLong;
+            try content_buffer.resize(header.content_length);
+            const actual_length = try reader.readAll(content_buffer.items);
+            if (actual_length < header.content_length) return error.UnexpectedEof;
+            break :blk content_buffer.items;
+        };
+
+        try logger.println(content);
     }
 
     try logger.println("Stopped AutoIt Language Server");
 }
 
-fn parseHeaders(bytes: []const u8) !struct { conteng_length: u32 } {
+fn parseHeaders(bytes: []const u8) !struct { content_length: u32 } {
     var content_length: ?u32 = null;
 
     var lines = std.mem.splitScalar(u8, bytes, '\n');
@@ -36,7 +56,7 @@ fn parseHeaders(bytes: []const u8) !struct { conteng_length: u32 } {
         const trimmed = std.mem.trimRight(u8, line, &std.ascii.whitespace);
         if (trimmed.len == 0) continue;
 
-        const colon = try std.mem.indexOfScalar(u8, trimmed, ':');
+        const colon = std.mem.indexOfScalar(u8, trimmed, ':') orelse return error.InvalidHeader;
 
         const value = std.mem.trim(u8, trimmed[colon + 1 ..], &std.ascii.whitespace);
 
