cd08b8f2f543371d10a6a6a60689e9ee57a7c27b
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-10-16 06:15:28 +0700

    feat(handler): parse initialize params

--- src/handler.zig
+++ src/handler.zig
@@ -7,9 +7,24 @@ const Method = enum([]const u8) {
 };
 
 pub fn handleMessage(message: lsp.Request) !void {
-    if (std.mem.eql(u8, message.method, "initialize")) try initialize();
+    if (std.mem.eql(u8, message.method, "initialize")) try initialize(message.params);
 }
 
-fn initialize() !void {
-    try logger.println("Handle Init.");
+fn initialize(params_value: std.json.Value) !void {
+    const params = try std.json.parseFromValue(
+        lsp.InitializeParams,
+        std.heap.page_allocator,
+        params_value,
+        .{
+            .ignore_unknown_fields = true,
+        },
+    );
+    defer params.deinit();
+    const message = try std.fmt.allocPrint(
+        std.heap.page_allocator,
+        "{any}",
+        .{params.value.clientInfo},
+    );
+
+    try logger.println(message);
 }
